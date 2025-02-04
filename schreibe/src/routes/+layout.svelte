dd8f59c7bea8d2e80f6c5d24b4c1d2edfac65fda
Ajsi Sala <AjsSal20@htl-shkoder.com>
2025-02-04 11:56:40 +0100

    Meine erste Directory Aleine !

---
+++ schreibe/src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+<script>
+    import "../app.css";
+  </script>
+  
+  <header>
+    <h1>Meine Svelte Seite</h1>
+  </header>
+  
+  <main>
+    <slot />
+  </main>
+  
+  <footer>
+    <p>  Meine Erste Svelte Website, du bist Willkommen und hab sehr viel spaß!!!!!!</p>
+  </footer>
+  
---
+++ schreibe/src/routes/+page.svelte
@@ -0,0 +1,19 @@
+<script>
+    import { writable } from "svelte/store";
+  
+    let inputText = writable("");
+    let displayText = writable("Willkommen auf meiner Seite!");
+  
+    function updateText() {
+      displayText.set($inputText);
+    }
+  </script>
+  
+  <div>
+    <h1>{$displayText}</h1>
+    <h2>Gib einen neuen Text ein:</h2>
+  
+    <input type="text" bind:value={$inputText} placeholder="Neuer Text hier..." />
+    <button on:click={updateText}>Text ändern</button>
+  </div>
+  
